e0baae517c97133af4e835f6d32b18a50b0cb53b
klueless-io <david@ideasmen.com.au>
2021-03-31 20:03:10 +1100

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using P10MvcWithIdentity.Models;
+
+namespace P10MvcWithIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+      {
+      }
+
+      public DbSet<Person> Persons { get; set; }
+
+      public DbSet<Vehicle> Vehicles { get; set; }
+
+      // protected override void OnConfiguring(DbContextOptionsBuilder options)
+      //   => options.UseSqlServer("Server=localhost,1433; Database=P10;User=sa; Password=SomeDeveloperPassword");
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace P10MvcWithIdentity.Models
+{
+  public class Vehicle
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string Model { get; set; }
+
+  }
+}
